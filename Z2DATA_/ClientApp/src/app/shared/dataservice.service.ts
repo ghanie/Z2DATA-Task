58f968670c5a0379ea3842f6ee108e8acf82e441
Abdallah Abdulghany <abdallahabdulghany@gmail.com>
2020-12-06 00:06:38 +0200

    Data Service

--- Z2DATA_/ClientApp/src/app/shared/dataservice.service.ts
+++ Z2DATA_/ClientApp/src/app/shared/dataservice.service.ts
@@ -4,6 +4,13 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 
+interface IProcess {
+  id: number,
+  customerName: string,
+  goodsId: number,
+  quantity: number;
+}
+
 @Injectable(
    { 
    providedIn: 'root' 
@@ -38,57 +45,33 @@ export class DataService {
 
 
 
-  /* GET heroes whose name contains search term */
-  /*
-    searchHeroes(term: string): Observable<Hero[]> {
-      if (!term.trim()) {
-        // if not search term, return empty hero array.
-        return of([]);
-      }
-      return this.http.get<Hero[]>(`${this.AppUrl}/?name=${term}`).pipe(
-        tap(x => x.length ?
-           this.log(`found heroes matching "${term}"`) :
-           this.log(`no heroes matching "${term}"`)),
-        catchError(this.handleError<Hero[]>('searchHeroes', []))
-      );
-    }
-  */
 
   //////// Save methods //////////
 
-  /** POST: add a new hero to the server */
-  /*
-   addHero(hero: Hero): Observable<Hero> {
-     return this.http.post<Hero>(this.AppUrl, hero, this.httpOptions).pipe(
-       tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
-       catchError(this.handleError<Hero>('addHero'))
-     );
-   }
- */
-
-  /** DELETE: delete the hero from the server */
-  /*
-    deleteHero(hero: Hero | number): Observable<Hero> {
-      const id = typeof hero === 'number' ? hero : hero.id;
-      const url = `${this.AppUrl}/${id}`;
+  /** POST: add a new Order to the server */
   
-      return this.http.delete<Hero>(url, this.httpOptions).pipe(
-        tap(_ => this.log(`deleted hero id=${id}`)),
-        catchError(this.handleError<Hero>('deleteHero'))
-      );
-    }
-  */
+  addOrder(process:IProcess) {
+    return this.http.post(this.AppUrl + 'api/orders/Create', process)
+      .pipe(
+        map((response: Response) => response.json()))
+  } 
+ 
+
+  /** DELETE: delete the Order from the server */
+  
+  deleteOrder(id:number) {  
+    return this.http.delete(this.AppUrl + "api/Orders/Delete/" + id)  
+        .pipe(
+          map((response: Response) => response.json()));
+}  
+  
 
 
-  /** PUT: update the hero on the server */
-  /*
-    updateHero(hero: Hero): Observable<any> {
-      return this.http.put(this.AppUrl, hero, this.httpOptions).pipe(
-        tap(_ => this.log(`updated hero id=${hero.id}`)),
-        catchError(this.handleError<any>('updateHero'))
-      );
-    }
-  */
+updateOrder(process : IProcess) {  
+  return this.http.put(this.AppUrl + "api/Orders/Edit/"+ process.id, process)  
+      .pipe(
+        map((response: Response) => response.json()));  
+}  
 
 
   /**
